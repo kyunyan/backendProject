ed45fb83dc150a795d8d64f9593e799da6530654
leegeonwan <gwlee@exciform.com>
2021-02-25 19:47:34 +0900

    test code 작성

--- src/test/java/com/study/spring/elegantbrothers/controller/HelloWordControllerTest.java
+++ src/test/java/com/study/spring/elegantbrothers/controller/HelloWordControllerTest.java
@@ -3,6 +3,11 @@ package com.study.spring.elegantbrothers.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,9 +16,22 @@ class HelloWordControllerTest {
     @Autowired
     private HelloWordController helloWordController;
 
+    private MockMvc mockMvc;
+
     @Test
     void HellowWorld(){
-       // System.out.println("test");
+//        System.out.println("test");
         System.out.println(helloWordController.HelloWorld());
+    }
+
+    @Test
+    void mockMvcTest() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(helloWordController).build();
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/api/helloWorld")
+        ).andDo(MockMvcResultHandlers.print())
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string("helloWorld"));
     }
 }
